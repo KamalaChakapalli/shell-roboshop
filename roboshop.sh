e3629c74037313f6200a072ce4f5db64997401e7
Kamala Chakapalli <kamalach1719@gmail.com>
2025-05-21 23:33:16 +1000

    colors

--- roboshop.sh
+++ roboshop.sh
@@ -10,7 +10,7 @@ for instance in ${INSTANCES[0]}
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance, Tags=[{Key=name, Value=test}]" ---query "Instances[0].InstanceId" --output text)
 
-    if [ $instance != "frontend"]
+    if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
