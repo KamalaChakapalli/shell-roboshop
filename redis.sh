2ccde3cc37698c33bc8c91c1e4d84c624f791070
Kamala Chakapalli <kamalach1719@gmail.com>
2025-05-29 21:27:36 +1000

    roboshop

--- redis.sh
+++ redis.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 
-source ./common.sh
-app_name=redis
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/logs/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
 
-CHECK_ROOT
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $Log_File
+    exit 1 #you can give any value upto 127 other than 0
+else
+    echo "You are running with root access" | tee -a $Log_File
+fi
+
+#validate functions takes the exit status of previous command as input
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
 
 dnf module disable redis -y &>> $LOG_FILE
 VALIDATE $? "Disabling default Redis version"
@@ -23,4 +52,7 @@ VALIDATE $? "Enabling Redis"
 systemctl start redis &>> $LOG_FILE
 VALIDATE $? "Starting redis"
 
-PRINT_TIME
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+
